e668120d2d9dd4266ff1336bda2f465383a9233a
FaizaNuha <mubarokfaiza@gmail.com>
2024-07-14 21:53:57 +0600

    auth done

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:carbon/onboarding/signup_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+import 'onboarding/login_page.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+
+    print('Firebase initialized successfully');
+  } catch (e) {
+    print('Failed to initialize Firebase: $e');
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return const MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: LoginPage(),
+   );
+  }
+
+}
